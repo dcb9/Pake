b7ddc8000b7a7e9843e697134de32e104a929f9e
Chengbin Du <robertduu@gmail.com>
2024-10-07 12:30:22 +0800

    optimize search exp and hide nav

--- src-tauri/src/inject/custom.js
+++ src-tauri/src/inject/custom.js
@@ -19,4 +19,30 @@ document.addEventListener('DOMContentLoaded', () => {
   for (const i of list) {
     i.remove();
   }
+
+  const input = document.getElementById('searchword');
+  setTimeout(() => {
+    input.blur();
+  }, 10);
+
+
+  const nav = document.querySelector("#header :first-child");
+  nav.remove();
+
+  const header = document.querySelector("#header");
+  const main = document.querySelector("#header + div");
+  main.style.paddingTop = (header.offsetHeight + 5) + 'px';
 });
+
+document.addEventListener('keydown', (event) => {
+  window.scrollTo(0, 0);
+  const input = document.getElementById('searchword');
+  if (event.target == input) {
+    console.log(event);
+    return;
+  }
+
+  input.focus();
+  input.value = '';
+  console.log(event);
+})
